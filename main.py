f676d501d8788acab713fab90aee3d2635d8d57b
Juanariasl <leondavid1946@gmail.com>
2024-10-02 20:42:14 -0500

    main.py

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
+# main.py
 import clase
-import random
 
-# Función que realiza un cálculo aleatorio
-def calculo_aleatorio():
-    operaciones = ["square", "add", "sub"]
-    operacion = random.choice(operaciones)
+# Crear un objeto de RandomCalculator
+random_calculator = clase.RandomCalculator()
 
-    if operacion == "square":
-        valor = random.randint(1, 10)  # Número aleatorio entre 1 y 10
-        objeto_cuadrado = clase.Square(valor)
-        print(f"Operación: Cuadrado de {valor}")
-        print(f"Resultado: {objeto_cuadrado.getVal()}")
-    
-    elif operacion == "add":
-        a, b = random.randint(1, 10), random.randint(1, 10)
-        objeto_add_sub = clase.Add_Sub()
-        print(f"Operación: Suma de {a} + {b}")
-        print(f"Resultado: {objeto_add_sub.add(a, b)}")
-    
-    elif operacion == "sub":
-        a, b = random.randint(1, 10), random.randint(1, 10)
-        objeto_add_sub = clase.Add_Sub()
-        print(f"Operación: Resta de {a} - {b}")
-        print(f"Resultado: {objeto_add_sub.sub(a, b)}")
-
-# Ejecutando el cálculo aleatorio
-if __name__ == "__main__":
-    calculo_aleatorio()
+# Realizar un cálculo aleatorio
+result = random_calculator.calculate()
+print(result)
